9690d526fa94ec9841718086b64e6bceb76ba53c
Andressa Lira <andressa_lira@outlook.com>
2023-11-29 22:29:59 -0300

    updates

--- src/main/java/br/com/unipe/projeto/ProjetoFinal/repository/PalestranteRepository.java
+++ src/main/java/br/com/unipe/projeto/ProjetoFinal/repository/PalestranteRepository.java
@@ -1,11 +1,15 @@
 package br.com.unipe.projeto.ProjetoFinal.repository;
 
 import br.com.unipe.projeto.ProjetoFinal.model.Palestrante;
+import br.com.unipe.projeto.ProjetoFinal.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface PalestranteRepository {
+@Repository
+public interface PalestranteRepository extends JpaRepository<Palestrante, Integer> {
     Optional<Palestrante> findById(Integer idPalestrante);
 
-    void save(Palestrante palestrante);
+//    void save(Palestrante palestrante);
 }
